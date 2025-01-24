d8abd00c52f62e600a023bc027865006f2adf610
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2025-01-24 12:05:20 -0800

    Update conftest.py

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,7 +8,7 @@ from alphaswarm.config import Config
 
 @fixture
 def default_config() -> Config:
-    file = os.path.join(os.path.dirname(__file__), "..", "..", ".env.example")
+    file = os.path.join(os.path.dirname(__file__), "..", "..", ".env")
     assert os.path.isfile(file)
     dotenv.load_dotenv(file)
     return Config(network_env="all")
