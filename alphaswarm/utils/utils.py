0898e26aac81a0f0b9a0181f3304ce10b967c68b
Ethan <jackson.ethan.c@gmail.com>
2025-01-31 20:41:38 +0000

    change default strategy filename

--- alphaswarm/utils/utils.py
+++ alphaswarm/utils/utils.py
@@ -4,7 +4,7 @@ from alphaswarm.config import CONFIG_PATH
 
 
 def load_strategy_config() -> str:
-    strategy_path = os.path.join(CONFIG_PATH, "strategy_config.md")
+    strategy_path = os.path.join(CONFIG_PATH, "momentum_strategy_config.md")
     try:
         with open(strategy_path, "r", encoding="utf-8") as f:
             return f.read()
