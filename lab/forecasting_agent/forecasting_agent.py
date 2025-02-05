49a50928dec1b1c6a1491596fb0db41074b4ebb1
Daniel Kur <daniel.kur15@gmail.com>
2025-02-05 16:45:20 +0000

    Custom system prompt

--- lab/forecasting_agent/forecasting_agent.py
+++ lab/forecasting_agent/forecasting_agent.py
@@ -7,12 +7,12 @@ from alphaswarm.agent.clients import TerminalClient
 from alphaswarm.tools.alchemy import AlchemyPriceHistoryByAddress, AlchemyPriceHistoryBySymbol
 from alphaswarm.tools.cookie.cookie_metrics import CookieMetricsBySymbol
 from alphaswarm.config import Config, BASE_PATH
+from alphaswarm.utils.file_utils import read_text_file_to_string
 from lab.forecasting_agent.price_forecasting_tool import PriceForecastingTool
 from smolagents import Tool
 
 async def main() -> None:
     dotenv.load_dotenv()
-
 
     tools: List[Tool] = [
         AlchemyPriceHistoryBySymbol(),
@@ -20,16 +20,22 @@ async def main() -> None:
         PriceForecastingTool(),
     ]
 
-    hints = """
-    ## Tokens
-    These are the symbols and addresses of the tokens you are responsible for forecasting.
-    - AIXBT (base): 0x4F9Fd6Be4a90f2620860d680c0d4d5Fb53d1A825
-    - VIRTUAL (base): 0x0b3e328455c4059EEb9e3f84b5543F74E24e7E1b
-    - VADER (base): 0x731814e491571A2e9eE3c5b1F7f3b962eE8f4870
+    system_prompt = read_text_file_to_string(BASE_PATH / "lab/forecasting_agent/prompts/forecasting_agent_system_prompt.txt")
+
+    specialization = """
+    ## Specialization
+    You are specialized in making price forecasts for crypto assets.
+
+    When doing this:
+    1. Get the historical price data for the token
+    2. Use additional tools to get market context that is relevant to predicting the price of a token
+    3. Use may use more than one tool to get additional market context if needed and if available
+    4. Use the forecasting tool to generate a price forecast
     """
 
+    system_prompt = system_prompt.replace("{{specialization}}", specialization)
 
-    agent = AlphaSwarmAgent(tools=tools, model_id="anthropic/claude-3-5-sonnet-20240620", hints=hints)
+    agent = AlphaSwarmAgent(tools=tools, model_id="anthropic/claude-3-5-sonnet-20240620", system_prompt=system_prompt)
 
     terminal = TerminalClient("AlphaSwarm terminal", agent)
     await asyncio.gather(
