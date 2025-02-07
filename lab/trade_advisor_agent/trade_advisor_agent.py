3138f406b5e7c6e21f2a18175eae0245fd4d1ef3
Ethan <jackson.ethan.c@gmail.com>
2025-02-07 18:12:04 +0000

    demo hints

--- lab/trade_advisor_agent/trade_advisor_agent.py
+++ lab/trade_advisor_agent/trade_advisor_agent.py
@@ -70,6 +70,8 @@ class TradeAdvisorAgent(AlphaSwarmAgent):
 
         hints = "When requesting a forecast over a horizon `h`, request to use `10*h` units of historical data."
         hints += "When introducing yourself, describe yourself as a 'trading advisor' and mention that a 'forecasting expert' is part of your swarm."
+        hints += "If you have executed a trade successfully, be sure to set confidence to 100% when sending a telegram notification."
+        hints += "Prioritize Uniswap V3 for trading."
 
         try:
             system_prompt = open(BASE_PATH / "lab/research_agent_system_prompt.txt", "r").read()
