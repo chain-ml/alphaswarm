0816fcc890000b89f043fbb96f5de5a3d3063dbf
Arnaud Flament <arnaud.flament@gmail.com>
2025-02-07 16:38:10 -0800

    Add new API endpoint for AlchemyClient
    alchemy_getAssetTransfers

--- tests/integration/services/alchemy/test_alchemy_client.py
+++ tests/integration/services/alchemy/test_alchemy_client.py
@@ -1,5 +1,14 @@
 from datetime import datetime, timedelta, timezone
+
+import pytest
+
+from alphaswarm.config import ChainConfig, Config
 from alphaswarm.services.alchemy.alchemy_client import AlchemyClient
+
+
+@pytest.fixture
+def eth_sepolia_config(default_config: Config) -> ChainConfig:
+    return default_config.get_chain_config(chain="ethereum_sepolia")
 
 
 def test_historical_prices_by_symbol(alchemy_client: AlchemyClient) -> None:
@@ -31,3 +40,25 @@ def test_historical_prices_by_address(alchemy_client: AlchemyClient) -> None:
     assert 24 <= len(result.data) <= 25
     assert result.data[0].value > 0.1
     assert result.data[0].timestamp >= start
+
+
+@pytest.mark.skip("Needs a wallet")
+def test_get_incoming_transfer(alchemy_client: AlchemyClient, eth_sepolia_config: ChainConfig) -> None:
+    # Test outgoing transfers
+    transfers = alchemy_client.get_transfers(
+        wallet=eth_sepolia_config.wallet_address, chain=eth_sepolia_config.chain, incoming=False
+    )
+
+    assert len(transfers) > 0
+    assert transfers[0].from_address.lower() == eth_sepolia_config.wallet_address.lower()
+
+
+@pytest.mark.skip("Needs a wallet")
+def test_get_outcoming_transfer(alchemy_client: AlchemyClient, eth_sepolia_config: ChainConfig) -> None:
+    # Test outgoing transfers
+    transfers = alchemy_client.get_transfers(
+        wallet=eth_sepolia_config.wallet_address, chain=eth_sepolia_config.chain, incoming=True
+    )
+
+    assert len(transfers) > 0
+    assert transfers[0].to_address.lower() == eth_sepolia_config.wallet_address.lower()
