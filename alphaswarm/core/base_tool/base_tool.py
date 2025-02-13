27d8d1987d9b9539c837a1b48842e1063c36d9a0
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2025-02-13 16:38:04 -0500

    Make override optional

--- alphaswarm/core/base_tool/base_tool.py
+++ alphaswarm/core/base_tool/base_tool.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from typing import Any
 
 from smolagents import Tool
@@ -7,15 +8,20 @@ class AlphaSwarmBaseTool(Tool):
     """
     A wrapper around the smolagents tool interface.
 
-    Automatically sets the following attributes:
+    Automatically sets the following attributes if not already provided:
     - name: being class name
     - description: being docstring of the class
+    - output_type: being "object"
     """
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
         super().__init_subclass__(**kwargs)
-        cls.name = cls.__name__
-        cls.description = cls.__doc__
-        # TODO: only set if not provided?
 
-        # cls.output_type = "object"  # next step
+        if "name" not in cls.__dict__:
+            cls.name = cls.__name__
+        if "description" not in cls.__dict__:
+            if cls.__doc__ is None:
+                raise ValueError("Description of the tool must be provided either as docstring or as a class attribute")
+            cls.description = dedent(cls.__doc__).strip()
+        if "output_type" not in cls.__dict__:
+            cls.output_type = "object"
