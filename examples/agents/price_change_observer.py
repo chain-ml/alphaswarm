a95a067ac97ad068b597dfdf4902de92729c12fb
Ethan <jackson.ethan.c@gmail.com>
2025-02-13 15:57:55 +0000

    price change observer example

--- examples/agents/price_change_observer.py
+++ examples/agents/price_change_observer.py
@@ -1,37 +1,22 @@
 import asyncio
+import datetime
 import dotenv
-from enum import Enum
 import logging
 from typing import List, Optional
 
 from alphaswarm.agent.agent import AlphaSwarmAgent
-from alphaswarm.agent.clients import TerminalClient, CronJobClient
-from alphaswarm.config import BASE_PATH, Config
+from alphaswarm.agent.clients import CronJobClient
 from alphaswarm.tools.alchemy import TokenPriceChangeCalculator
 from alphaswarm.services.alchemy import AlchemyClient
 
-class PriceChangeInterval(Enum):
-    FIVE_MINUTES: str = "5m"
-    ONE_HOUR: str = "1h"
-    ONE_DAY: str = "1d"
-
-
-"""
-TODO: Finish this in the morning -- we just want the agent to execute a sequence of tool calls,
-with some logic to apply thresholds or whatever.
-
-The point is not that this is needed for this example, but rather that the pattern still works.
-
-In another example, we'll have an agent that is generating forecasts, which could absoultely use the 'agentic' pattern. 
-"""
 
 class PriceChangeObserver(AlphaSwarmAgent):
     def __init__(
         self,
         token_addresses: List[str],
         chain: str = "base-mainnet",
-        price_change_interval: str = PriceChangeInterval.ONE_HOUR,
-        price_change_percentage: float = 2.0,
+        price_change_interval: str = "1h",
+        price_pct_chg_thresh: float = 2.0,
     ) -> None:
         """
         Initialize the PriceChangeObserver with some `configurable` parameters.
@@ -42,7 +27,7 @@ class PriceChangeObserver(AlphaSwarmAgent):
             token_addresses: List of token addresses to observe
             chain: Chain to observe
             price_change_interval: Interval to observe
-            price_change_percentage: Percentage to observe
+            price_pct_chg_thresh: Percentage change threshold to observe
         """
 
         self.alchemy_client = AlchemyClient.from_env()
@@ -50,32 +35,47 @@ class PriceChangeObserver(AlphaSwarmAgent):
         self.token_addresses = token_addresses
         self.chain = chain
         self.price_change_interval = price_change_interval
-        self.price_change_percentage = price_change_percentage
+        self.price_pct_chg_thresh = price_pct_chg_thresh
 
         super().__init__(tools=[self.price_change_calculator])
 
-    async def process_message(self, current_message: str) -> Optional[str]:
-        # It doesn't matter what the message is, we just want to 'execute' a sequence of tool calls
-        logging.info(f"Processing message: {current_message}")
-
-        # Get the price history
+    def get_price_alerts(self) -> str:
+        """
+        Get the price alerts for the token addresses.
+        """
         price_alerts = []
         for address in self.token_addresses:
-            logging.info(f"Processing token address: {address}")
             price_history = self.price_change_calculator.forward(
                 token_address=address,
                 frequency=self.price_change_interval,
                 n_samples=2,
                 network=self.chain,
-            )
-            logging.info(f"Price history: {price_history}")
-            if price_history.percent_change >= self.price_change_percentage:
-                price_alerts.append(f"Price alert for {address}: {price_history.percent_change}% change in the last {self.price_change_interval}.")
+            )  
+            
+            if abs(price_history.percent_change) >= self.price_pct_chg_thresh:
+                price_alerts.append(f"{address}: {price_history.percent_change}% change in the last {self.price_change_interval}.")
         
+        logging.info(f"{len(price_alerts)} price alerts found.")
         if len(price_alerts) > 0:
-            return "\n".join(price_alerts)
+            alert_message = f"Price changes have been observed for the following tokens as of {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: "
+            alert_message += "\n" + "\n".join(price_alerts)
+            return alert_message
         else:
-            return "No price alerts found."
+            return ""
+            
+
+    async def process_message(self, message: str) -> Optional[str]:
+        """
+        Add your logic here to decide what to do with the message.
+        
+        Args:
+            message: Previous messages followed by the current message that includes the price alerts.
+
+        Returns:
+            The agent's response to the message.
+        """
+        logging.info(f"Agent received alerts: {message}")
+        return None
 
 
 async def main() -> None:
@@ -90,16 +90,17 @@ async def main() -> None:
     agent = PriceChangeObserver(
         token_addresses=token_addresses,
         chain="base-mainnet",
-        price_change_interval="1h",
-        price_change_percentage=2.0,
+        price_change_interval="5m",
+        price_pct_chg_thresh=0.2,
     )
 
     cron_client = CronJobClient(
         agent=agent,
-        client_id="AlphaSwarm Observer Example",
+        client_id="Price Change Observer",
         interval_seconds=5,
-        message_generator=lambda: "Check the price history for any alerts.",
-        should_process=lambda _: True,
+        response_handler=lambda _: None,
+        message_generator=agent.get_price_alerts,
+        should_process=lambda alerts: len(alerts) > 0,
         skip_message=lambda _: None,
     )
     await asyncio.gather(cron_client.start())
